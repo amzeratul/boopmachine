654b90b95a4d22c48a0470f24fab45a9bf449581
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2017-12-07 19:55:42 +0000

    Add license.

--- boop_machine/boop_machine.ino
+++ boop_machine/boop_machine.ino
@@ -1,5 +1,5 @@
 /*
- * 
+
 Copyright 2017 Rodrigo Braz Monteiro
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
